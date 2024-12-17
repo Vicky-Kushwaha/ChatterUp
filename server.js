8109110467f25279878f644e3538fe44deec0ea3
Vicky-Kushwaha <muzvickykerma12345@gmail.com>
2024-12-17 14:03:33 +0530

    change port no

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ import { appLevelErrorHandlerMiddleware } from "./src/middlewares/errorHandler.j
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const server = http.createServer(app);
 app.use(express.static(path.join(__dirname, "public")));
